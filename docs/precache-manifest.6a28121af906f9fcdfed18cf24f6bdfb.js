1e28d6e838be726567c527a6cc1a75d160fd18d8
Rafael Ramalho <rafazelramalho19@gmail.com>
2019-10-18 16:50:02 +0100

    add githubpages

---
+++ docs/precache-manifest.6a28121af906f9fcdfed18cf24f6bdfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ea3f77028cdac1682c2",
+    "url": "/animations-presentation/static/css/main.c773e1e4.chunk.css"
+  },
+  {
+    "revision": "2ea3f77028cdac1682c2",
+    "url": "/animations-presentation/static/js/main.2ea3f770.chunk.js"
+  },
+  {
+    "revision": "beca3ee73041c174342b",
+    "url": "/animations-presentation/static/css/1.db5133b6.chunk.css"
+  },
+  {
+    "revision": "beca3ee73041c174342b",
+    "url": "/animations-presentation/static/js/1.beca3ee7.chunk.js"
+  },
+  {
+    "revision": "60f398ad75bb66d4b613",
+    "url": "/animations-presentation/static/js/runtime~main.60f398ad.js"
+  },
+  {
+    "revision": "4442b57b9208bf8fbc70f3fd3340a23d",
+    "url": "/animations-presentation/index.html"
+  }
+];
